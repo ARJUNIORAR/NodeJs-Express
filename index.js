27abf599ac288d8777aec0505354e0451d6e5b32
ARJUNIORAR <141863248+ARJUNIORAR@users.noreply.github.com>
2025-04-29 20:01:47 -0300

    servidor

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const router = require("./rotas/index")
+const seque = require('./rotas/conexao')
 
 router(app);
 
-app.listen(port,(error) => {
+app.listen(port,async(error) => {
     if(error) {
         console.log("Deu erro");
         return;
     }
     console.log("Aplicação rodando");
+    try {
+        await seque.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
 });
+
